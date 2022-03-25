d92135983659ca09b0534ffd0ba0a7e2a5c30ad5
Do Vien <vienpro2506@gmail.com>
2022-03-25 23:21:54 +0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Đỗ Viên on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
